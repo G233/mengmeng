0d18a523b3fed9c05712799d1281847c3e46da60
lmm <2107405490@qq.com>
2023-02-06 17:00:31 +0800

    新增互动

--- my.js
+++ my.js
@@ -11,3 +11,13 @@ function handle_click(element){
     alert("叫你按你还真按啊？")
 
 }
+var img = document.getElementById("img") 
+console.log("img")
+img.addEventListener("mouseover",function(){
+    this.src = "小黑子2.png"
+
+})
+img.addEventListener("mouseout",function(){
+    this.src = "小黑子.jpg"
+
+})
