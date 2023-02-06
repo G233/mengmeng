fa92141662c187eb35c2050e990a378df6d19340
lmm <2107405490@qq.com>
2023-02-06 15:46:19 +0800

    有趣的按钮

--- my.js
+++ my.js
@@ -1,3 +1,13 @@
-function handle_click(){
-    alert("叫你按你就按啊？")
+
+var btn = document.getElementById("btn")
+btn.addEventListener("click",function(){
+   this.innerText = "按屁啊！"
+   this.style.color = "red"
+})
+
+this.innertext = "按屁啊！"
+
+function handle_click(element){
+    alert("叫你按你还真按啊？")
+
 }
