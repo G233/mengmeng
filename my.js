4dd4a24fb08015bb76c2f66d4752518a1b9063e4
lmm <2107405490@qq.com>
2023-03-02 13:07:11 +0800

    修改内容

--- my.js
+++ my.js
@@ -1,11 +1,11 @@
 
 var btn = document.getElementById("btn")
 btn.addEventListener("click",function(){
-   this.innerText = "按屁啊！"
+   this.innerText = "按错了！"
    this.style.color = "red"
 })
 
-this.innertext = "按屁啊！"
+this.innertext = "按错了！"
 
 function handle_click(element){
     alert("叫你按你还真按啊？")
